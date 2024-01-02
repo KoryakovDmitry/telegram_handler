7ddda126a140efde56f72e76bc966d3ba72d19f2
dmitrii <dm.koryakov@gmail.com>
2024-01-02 19:11:50 +0100

    upd telegram handler

--- telegram_handler/handler.py
+++ telegram_handler/handler.py
@@ -19,13 +19,37 @@ logger = logging.getLogger(__name__)
 
 
 class TelegramLoggingHandler(logging.Handler):
-    def __init__(self, bot_token: str, channel: Union[str, int], level=logging.NOTSET):
+    EMOJI_MAP = {
+        logging.DEBUG: "DEBUG: \u26aa",
+        logging.INFO: "INFO: \U0001f535",
+        logging.WARNING: "WARNING: \U0001F7E0",
+        logging.ERROR: "ERROR: \U0001F534",
+        logging.CRITICAL: "CRITICAL: \U0001f525",
+    }
+
+    def __init__(
+        self,
+        bot_token: str,
+        channel: Union[str, int],
+        level=logging.NOTSET,
+        use_emoji: bool = True,
+        emoji_map: dict = None,
+    ):
         super().__init__(level)
         self._url = TelegramLoggingHandler._format_url(bot_token, channel)
         self._buffer = MessageBuffer(MAX_BUFFER_SIZE)
         self._stop_signal = RLock()
         self._writer_thread = None
         self._start_writer_thread()
+        self.use_emoji = use_emoji
+        self.emojis = self.EMOJI_MAP
+        if emoji_map:
+            self.emojis.update(emoji_map)
+
+    def format(self, record):
+        if self.use_emoji and record.levelno in self.emojis:
+            record.levelname = self.emojis[record.levelno]
+        return super().format(record)
 
     @staticmethod
     def _format_url(bot_token: str, channel: Union[str, int]):
